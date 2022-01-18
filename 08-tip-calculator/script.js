c7e3a394f6cc4fcdf5d58462ceffdd6851d2b2ef
Denis Kuznetsov <denispr89@gmail.com>
2022-01-18 06:34:08 +0300

    tip calc

--- 08-tip-calculator/script.js
+++ 08-tip-calculator/script.js
@@ -1,20 +1,43 @@
 
-const output = document.querySelector('output')
+const output = document.querySelector('.display__output')
 
 const div = document.createElement('div');
 div.classList.add('buttons__keyboard');
 document.querySelector('.buttons__body').appendChild(div);
 
 const calcButtons = 'C CE % / 7 8 9 * 4 5 6 - 1 2 3 + 0 ( ) =';
-
 calcButtons.split(' ').map(symbol => {
-    div.insertAdjacentHTML('beforeend', `<button class="button" value="${symbol}">${symbol}</button>`)
-})
+    div.insertAdjacentHTML('beforeend', `<button class="button" value="${symbol}">${symbol}</button>`);
+});
+
+let btnValues = document.querySelectorAll('.button');
+console.log(btnValues.classList);
+
+if (btnValues.value === '+') {
+    btnValues.classList.add('button-plus', 'option-btn');
+   
+}
+
 
 document.querySelectorAll('.button').forEach(button => {
-    button.addEventListener('click', function () {
-        console.log(this.value);
-       document.querySelector('.display__output').textContent += this.value;
-       
-    })
-})
+    button.addEventListener('click', function () {        
+        if (this.value === 'C') {
+            output.innerHTML = '';
+        } else if (this.value === 'CE') {
+            output.innerHTML = output.innerHTML.substring(0, output.innerHTML.length - 1);
+        } else {
+            output.innerHTML += this.value;
+        }
+
+        const currentNum = output.value;
+        console.log(currentNum);
+        
+        const moveNum = function () {
+            oldNum = currentNum;
+            currentNum = '';
+
+        }
+    });
+});
+
+
